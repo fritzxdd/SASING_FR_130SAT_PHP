b2a8f977ae3dfbda3d95dbe974bb81fec0f61d98
fritz <fritz.marlowe@gmail.com>
2025-02-09 20:57:04 +0800

    0204_5 set the navigation link

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>My Website</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-    
-    {{ $slot }}
+<body class="h-full">
+<div class="min-h-full">
+  <nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-10 flex items-baseline space-x-4">
+              <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+            </div>
+          </div>
+        </div>
 </body>
 </html>
