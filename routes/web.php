ff4299921a9543598b00682d5fa946f35e639181
fritz <fritz.marlowe@gmail.com>
2025-02-09 20:36:26 +0800

    0204_3 create the obligatory three page layout using layout files

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
-    return view('About');
+    return view('about');
 });
